ae9dfc78a88fcc0676dc47b34e16557f3160df9a
Adam Toth7 <adam_toth7@epam.com>
2022-09-12 11:09:15 +0000

    KUKA-0041_Fix_Docker_sql_init_problem

--- persistence/data.sql
+++ persistence/data.sql
@@ -5,3 +5,9 @@ INSERT INTO Address  (zipCode, city, street_name, address_type, number) VALUES
 	('1231',		'Alsómocsolád',		'Fabatka',  'utca',	'23'),
 	('H-9231',	    'Homokbödöge',		'Kalapács', 'dűlő',	'13-16'),
 	('4532',		'Vindornyalak',		'Fúró',     'tér',	'13/b');
+INSERT INTO Container_size (type) VALUES ('5m3'),('8m3'),('10m3');
+INSERT INTO Waste (type) VALUES ('építési_törmelék'),('veszélyes_hulladék'),('lom'),('kevert_hulladék');
+INSERT INTO Container  (size, is_booked) VALUES
+	('5m3',     FALSE),
+	('8m3',     FALSE),
+	('10m3',    TRUE);
